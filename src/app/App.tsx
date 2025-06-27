dcab4b6a20406ec7d3febb31d882ea0aee5cac07
bira10a <94951002+bira10a@users.noreply.github.com>
2025-06-27 17:12:26 +0300

    work ysa, added ...

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import './styles/index.scss';
 import {Routes, Route} from 'react-router';
 import { Suspense, useContext, useState } from 'react';
-import { ThemeContext } from './themes/ThemeContext';
-import { Themes } from './themes/ThemeContext';
-import { useTheme } from './themes/useTheme';
+import { ThemeContext } from '../themes/ThemeContext';
+import { Themes } from '../themes/ThemeContext';
+import { useTheme } from '../themes/useTheme';
 
-import { Layout } from "./components/Layout";
+import { Layout } from "../components/Layout";
 // import { Home } from "./pages/Home/Home";
-import { HomeAsync } from './pages/Home/Home.async';
+import { HomeAsync } from '../pages/Home/Home.async';
 // import { Moto } from "./pages/Moto/Moto";
-import { MotoAsync } from "./pages/Moto/Moto.async";
+import { MotoAsync } from "../pages/Moto/Moto.async";
 // import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
-import { NotFoundPageAsync } from "./pages/NotFoundPage/NotFoundPage.async";
+import { NotFoundPageAsync } from "../pages/NotFoundPage/NotFoundPage.async";
 
 
 const App = () => {
